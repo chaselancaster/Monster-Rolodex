d00fa395d365b15af0687874c9fc64e340bd7e48
chaselancaster <chaseblancaster@gmail.com>
2019-11-01 16:38:50 -0700

    add props.children in returning div in card-list

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name="Chase" />
+        <CardList name="Chase">
+          <h1>Chase</h1>
+        </CardList>
         {this.state.monsters.map(monster => (
           <h1 key={monster.id}> {monster.name} </h1>
         ))}
