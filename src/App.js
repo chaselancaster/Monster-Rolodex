4d1a72eef22e795d748cadc342964a2ffb0dd073
chaselancaster <chaseblancaster@gmail.com>
2019-11-01 16:33:05 -0700

    add Components folder, card-list folder, and card-list component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
   };
 
   componentDidMount() {
+    // Fetching the data and taking the response and turning it in JSON. Then we take the users and set our monsters to that array of users.
     fetch("https://jsonplaceholder.typicode.com/users").then(response =>
       response.json().then(users => this.setState({ monsters: users }))
     );
