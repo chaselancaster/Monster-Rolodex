e0941d5e76fc5c2ee82305380bb52b1609c8dfd2
chaselancaster <chaseblancaster@gmail.com>
2019-11-04 15:56:13 -0800

    add size parameter to robohash link

--- src/Components/card/card.component.jsx
+++ src/Components/card/card.component.jsx
@@ -6,8 +6,8 @@ export const Card = props => (
   <div className="card-container">
     <img
       alt="monster"
-      src={`https://robohash.org/${props.monster.id}?set=set2`}
-    ></img>
+      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+    />
     <h1 key={props.monster.id}> {props.monster.name} </h1>
   </div>
 );
