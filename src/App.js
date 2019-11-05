acb9cbb7eb1bb673f5cde4ca99d5a2b76a59556b
chaselancaster <chaseblancaster@gmail.com>
2019-11-04 16:49:43 -0800

    add input search bar

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input type="search" placeholder="search monsters" />
         <CardList monsters={this.state.monsters}></CardList>
       </div>
     );
