ea4151c345ac5e033fc4009f01752cba392ce17d
chaselancaster <chaseblancaster@gmail.com>
2019-11-05 17:16:31 -0800

    destruture handleChange and export SearchBox

--- src/Components/search-box/search-box.component.jsx
+++ src/Components/search-box/search-box.component.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./search-box.styles.css";
 
-const SearchBox = ({ placeholder }) => (
+export const SearchBox = ({ placeholder, handleChange }) => (
   <input
     className="search"
     type="search"
     placeholder={placeholder}
-    onChange={e => this.setState({ searchField: e.target.value })}
+    onChange={handleChange}
   />
 );
