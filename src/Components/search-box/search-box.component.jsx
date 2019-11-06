a68ab8cec11508f2e65a3c1f76b0997977a118ba
chaselancaster <chaseblancaster@gmail.com>
2019-11-05 17:15:48 -0800

    destructure placeholder in input box

--- src/Components/search-box/search-box.component.jsx
+++ src/Components/search-box/search-box.component.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./search-box.styles.css";
 
-const SearchBox = () => (
+const SearchBox = ({ placeholder }) => (
   <input
     className="search"
     type="search"
-    placeholder="search monsters"
+    placeholder={placeholder}
     onChange={e => this.setState({ searchField: e.target.value })}
   />
 );
