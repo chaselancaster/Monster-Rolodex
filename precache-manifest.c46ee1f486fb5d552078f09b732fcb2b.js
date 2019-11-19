44496836037a3ef4485ddce916e9987654d675ff
chaselancaster <chaseblancaster@gmail.com>
2019-11-18 17:22:32 -0800

    Updates

---
+++ precache-manifest.c46ee1f486fb5d552078f09b732fcb2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59265ad588420964a5fc7868df1239ce",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "991f62c65593e45c0a9f",
+    "url": "/monster-rolodex/static/css/main.76042869.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monster-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "991f62c65593e45c0a9f",
+    "url": "/monster-rolodex/static/js/main.bc9504dd.chunk.js"
+  },
+  {
+    "revision": "78872b30b8d5e18a4390",
+    "url": "/monster-rolodex/static/js/runtime-main.716ed2c9.js"
+  }
+]);
